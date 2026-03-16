fa70a80fa597520a046629af666e6393f74c94df
Ashley James <25390807+dashmage@users.noreply.github.com>
2026-03-16 11:26:28 +0530

    Better #15 3sum

--- two_pointers/15-3sum.py
+++ two_pointers/15-3sum.py
@@ -4,12 +4,12 @@ Medium
 
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
- 
+
 Example 1:
 
 Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
-Explanation: 
+Explanation:
 nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
 nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
 nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
@@ -28,7 +28,7 @@ Input: nums = [0,0,0]
 Output: [[0,0,0]]
 Explanation: The only possible triplet sums up to 0.
 
- 
+
 Constraints:
 
 3 <= nums.length <= 3000
@@ -40,32 +40,46 @@ Constraints:
 
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        result = []
+        triplets: list[list[int]] = []
         nums.sort()
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            # triplets consisting of postive nums will never sum to 0
+            if nums[i] > 0:
+                break
+            # Avoid duplicate triplets, skip 'a' if it's the same as previous num
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            # keeping nums[i] constant
-            # solving two sum ii (#167)
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                three_sum = nums[i] + nums[l] + nums[r]
-                if three_sum > 0:
-                    r -= 1
-                elif three_sum < 0:
-                    l += 1
-                else:
-                    result.append([nums[i], nums[l], nums[r]])
-                    # move to the next non-duplicate element
-                    l += 1
-                    while nums[l] == nums[l-1] and l < r:
-                        l += 1
-        return result
+            # Find all pairs that sum to a target of '-a' (-nums[i])
+            pairs = pair_sum_sorted_all_pairs(nums, i + 1, -nums[i])
+            for pair in pairs:
+                triplets.append([nums[i]] + pair)
+        return triplets
+
+
+def pair_sum_sorted_all_pairs(
+    nums: list[int], start: int, target: int
+) -> list[list[int]]:
+    pairs = []
+    left, right = start, len(nums) - 1
+    while left < right:
+        sum = nums[left] + nums[right]
+        if sum == target:
+            pairs.append([nums[left], nums[right]])
+            left += 1
+            # Avoid duplicate [b, c] pairs, skip 'b' if it's the
+            # same as prev number
+            while left < right and nums[left] == nums[left - 1]:
+                left += 1
+        elif sum < target:
+            left += 1
+        else:
+            right -= 1
+    return pairs
+
 
 def validate(actual, expected):
     from collections import Counter
+
     actual_converted = set(str(x) for x in (Counter(triplet) for triplet in actual))
     expected_converted = set(str(x) for x in (Counter(triplet) for triplet in expected))
     return actual_converted == expected_converted
-    
